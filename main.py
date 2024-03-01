2666fdf1ba343061d507b6cf222e3f45917dca87
Lorenz Timothy Barco Ranera <lbranera@up.edu.ph>
2024-03-01 17:29:06 +0800

    simplified file writing and created row specific function

--- main.py
+++ main.py
@@ -60,40 +60,116 @@ def create_travel_info_matrix(params):
         cost_matrix = cost_matrix + ",".join(cost_single_row) + "\n"
 
     if (mode.upper() == "AIR"):
-        file = open(f"./result/land_{mode}_travel_time_matrix.csv", "w")
-        file.write(time_matrix)
-        file.close()
-
-        file = open(f"./result/land_{mode}_travel_distance_matrix.csv", "w")
-        file.write(distance_matrix)
-        file.close()
-
-        file = open(f"./result/land_{mode}_travel_cost_matrix.csv", "w")
-        file.write(cost_matrix)
-        file.close()
+        filenames = [
+            "./result/land_air_travel_time_matrix.csv",
+            "./result/land_air_travel_distance_matrix.csv",
+            "./result/land_air_travel_cost_matrix.csv"
+        ]
     else:
-        file = open(f"./result/land_{mode}_{pass_through}_travel_time_matrix.csv", "w")
-        file.write(time_matrix)
+        filenames = [
+            f"./result/land_sea_{pass_through}_travel_time_matrix.csv",
+            f"./result/land_sea_{pass_through}_travel_distance_matrix.csv",
+            f"./result/land_sea_{pass_through}_travel_cost_matrix.csv"
+        ]
+
+    file_content = [
+        time_matrix,
+        distance_matrix,
+        cost_matrix,
+    ]
+
+    for index, filename in enumerate(filenames):
+        target_file_content = file_content[index]
+
+        file = open(filename, "w")
+        file.write(target_file_content)
         file.close()
 
-        file = open(f"./result/land_{mode}_{pass_through}_travel_distance_matrix.csv", "w")
-        file.write(distance_matrix)
-        file.close()
 
-        file = open(f"./result/land_{mode}_{pass_through}_travel_cost_matrix.csv", "w")
-        file.write(cost_matrix)
-        file.close()
+def create_travel_info_row(source):        
+    
+    travel_modes = [
+        ("air", None),
+        ("sea", "hil"),
+        ("sea", "orm"),
+    ]
+
+    for mode, pass_through in travel_modes:
+        travel_time_list = []
+        travel_distance_list = []
+        travel_cost_list = []
+
+        for rtc_destination in rtc_list:
+            travel_info = get_travel_info(source, rtc_destination, mode, pass_through)
+
+            formatted_travel_time = str(travel_info["time"])
+            formatted_travel_distance = str(travel_info["distance"])
+            formatted_travel_cost = str(travel_info["cost"])
+
+            travel_time_list.append(formatted_travel_time)
+            travel_distance_list.append(formatted_travel_distance)
+            travel_cost_list.append(formatted_travel_cost)
+
+            print("SOURCE:", source)
+            print("DESTINATION:", rtc_destination)
+
+        # Done travel info computation
+
+        if (mode == "air"):
+            filenames = [
+                ("./result/land_air_travel_time_matrix.csv"),
+                ("./result/land_air_travel_distance_matrix.csv"),
+                ("./result/land_air_travel_cost_matrix.csv"),
+            ]
+        else:
+            filenames = [
+                f"./result/land_sea_{pass_through}_travel_time_matrix.csv",
+                f"./result/land_sea_{pass_through}_travel_distance_matrix.csv",
+                f"./result/land_sea_{pass_through}_travel_cost_matrix.csv",
+            ]
+
+
+            travel_info_list = [
+                travel_time_list,
+                travel_distance_list,
+                travel_cost_list,
+            ]
+
+            for index, filename in enumerate(filenames):
+                file = open(filename)
+                row_entries = [entry for entry in file.readlines()]
+                file.close()
+
+                target_row_index = 0
+                for row_index, row_entry in enumerate(row_entries):
+                    if (source.upper() in row_entry.upper()):
+                        target_row_index = row_index
+                        break
+
+                target_travel_info_list = travel_info_list[index]
+
+                updated_row = f'"{source}",' + ",".join(target_travel_info_list) + "\n"
+                row_entries[target_row_index] = updated_row
+
+                updated_content = "".join(row_entries)
+
+                file = open(filename, "w")
+                file.write(updated_content)
+                file.close()
+
 
 if __name__ == "__main__":
-    p1 = Process(target=create_travel_info_matrix, args=(("air", ""),))
-    p2 = Process(target=create_travel_info_matrix, args=(("sea", "hil"),))
-    p3 = Process(target=create_travel_info_matrix, args=(("sea", "orm"),))
+    #p1 = Process(target=create_travel_info_matrix, args=(("air", None),))
+    #p2 = Process(target=create_travel_info_matrix, args=(("sea", "hil"),))
+    #p3 = Process(target=create_travel_info_matrix, args=(("sea", "orm"),))
 
-    p1.start()
-    p2.start()
-    p3.start()
+    #p1.start()
+    #p2.start()
+    #p3.start()
 
-    p1.join()
-    p2.join()
-    p3.join()
+    #p1.join()
+    #p2.join()
+    #p3.join()
+    
+    create_travel_info_row(source = "Allen,Northern Samar")
 
