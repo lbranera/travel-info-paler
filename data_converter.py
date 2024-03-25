260292ad3810f7923ce59289f7924b42ecee96e8
Lorenz Timothy Barco Ranera <lbranera@up.edu.ph>
2024-03-25 16:35:39 +0800

    updated data_converter.py

--- data_converter.py
+++ data_converter.py
@@ -1,6 +1,7 @@
 # Converts seconds to string hour-and-minutes format
 
 def seconds_to_hours_minutes(seconds):
+    seconds = float(seconds)
     if seconds < 0:
         return "Invalid input, please provide a non-negative number of seconds."
 
@@ -12,5 +13,5 @@ def seconds_to_hours_minutes(seconds):
 # Converts meters to kilometers format
 
 def meters_to_kilometers(meters):
-    kilometers = meters / 1000
+    kilometers = float(meters) / 1000
     return kilometers
